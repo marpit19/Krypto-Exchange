baaae27c3b9c9442e22bf87752b80ccb3be581cc
Arpit Mohapatra <arpit.mohapatra19@gmail.com>
2022-03-06 02:17:57 +0530

    added header section

---
+++ client/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+
+const style = {
+  wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#1B3A4B] text-white select-none flex flex-col justify-between`
+}
+
+export default function Home() {
+  return (
+    <div className={style.wrapper}>
+      <Header/>
+      <h2>Main</h2>
+      <h2>Transaction History</h2>
+    </div>
+  )
+}
