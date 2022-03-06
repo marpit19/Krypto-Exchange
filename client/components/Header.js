e7be042e4416a6db7f3fa92fcda1ebd589489984
Arpit Mohapatra <arpit.mohapatra19@gmail.com>
2022-03-06 22:00:04 +0530

    added transaction context

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import Image from 'next/image'
 import { FiArrowUpRight } from 'react-icons/fi'
 import { AiOutlineDown } from 'react-icons/ai'
 import { HiOutlineDotsVertical } from 'react-icons/hi'
 import ethLogo from '../assets/eth.png'
 import logo from '../assets/logo.png'
+import { TransactionContext } from '../context/TransactionContext'
 
 const style = {
   wrapper: `p-4 w-screen flex justify-between items-center`,
@@ -23,6 +24,9 @@ const style = {
 
 const Header = () => {
   const [selectedNav, setSelectedNav] = useState('swap')
+  const {connectWallet, currentAccount} = useContext(TransactionContext)
+
+  console.log({connectWallet, currentAccount})
 
   return (
     <div className={style.wrapper}>
---
+++ client/context/TransactionContext.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+
+export const TransactionContext = React.createContext()
+
+let eth
+
+if (typeof window !== 'undefined') {
+  eth = window.ethereum
+}
+
+export const TransactionProvider = ({ children }) => {
+  const [currentAccount, setCurrentAccount] = useState()
+
+  useEffect(() => {
+    checkIfWalletIsConnected()
+  },[])
+
+  const connectWallet = async (metamask = eth) => {
+    try {
+      if (!metamask) return alert('Please install metamask')
+      const accounts = await metamask.request({ method: 'eth_requestAccounts' })
+      setCurrentAccount(accounts[0])
+    } catch (error) {
+      console.log(error)
+      throw new Error('No Ethereum object :(')
+    }
+  }
+
+  const checkIfWalletIsConnected = async (metamask = eth) => {
+    try {
+      if (!metamask) return alert('Please install metamask ')
+
+      const accounts = await metamask.request({ method: 'eth_accounts' })
+
+      if (accounts.length) {
+        setCurrentAccount(accounts[0])
+        console.log('Wallet is already connected')
+      }
+    } catch (error) {
+      console.error(error)
+      throw new Error('No ethereum object :(')
+    }
+  }
+
+  return (
+    <TransactionContext.Provider
+      value={{
+        currentAccount,
+        connectWallet,
+      }}
+    > 
+      {children}
+    </TransactionContext.Provider>
+  )
+}
